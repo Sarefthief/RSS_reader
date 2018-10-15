ce76a4439efd1a7d033991f04ba227fb22aeeda5
Sarefthief <Sarefthief@gmail.com>
2018-10-15 13:37:21 +0700

    XmlParser redacted

--- app/src/main/java/com/saref/rss_reader/XmlParser.java
+++ app/src/main/java/com/saref/rss_reader/XmlParser.java
@@ -13,7 +13,8 @@ public final class XmlParser
 {
     private static final String ns = null;
 
-    public ArrayList parse(InputStream in) throws XmlPullParserException, IOException {
+    public ArrayList parse(InputStream in) throws XmlPullParserException, IOException
+    {
         try {
             XmlPullParser parser = Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
@@ -25,25 +26,26 @@ public final class XmlParser
         }
     }
 
-    private ArrayList readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private ArrayList readFeed(XmlPullParser parser) throws XmlPullParserException, IOException
+    {
         ArrayList feedItems = new ArrayList();
 
-        parser.require(XmlPullParser.START_TAG, ns, "feed");
         while (parser.next() != XmlPullParser.END_TAG) {
             if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
             }
             String name = parser.getName();
             if (name.equals("item")) {
-                feedItems.add(readEntry(parser));
+                feedItems.add(readItem(parser));
             } else {
                 skip(parser);
             }
         }
         return feedItems;
     }
-    private FeedItem readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, ns, "entry");
+    private FeedItem readItem(XmlPullParser parser) throws XmlPullParserException, IOException
+    {
+        parser.require(XmlPullParser.START_TAG, ns, "item");
         String title = null;
         String description = null;
         String link = null;
@@ -70,14 +72,16 @@ public final class XmlParser
         return new FeedItem(title, description, link);
     }
 
-    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException
+    {
         parser.require(XmlPullParser.START_TAG, ns, "title");
         String title = readText(parser);
         parser.require(XmlPullParser.END_TAG, ns, "title");
         return title;
     }
 
-    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
+    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException
+    {
         String link = "";
         parser.require(XmlPullParser.START_TAG, ns, "link");
         String tag = parser.getName();
@@ -92,14 +96,16 @@ public final class XmlParser
         return link;
     }
 
-    private String readDescription(XmlPullParser parser) throws IOException, XmlPullParserException {
+    private String readDescription(XmlPullParser parser) throws IOException, XmlPullParserException
+    {
         parser.require(XmlPullParser.START_TAG, ns, "description");
         String summary = readText(parser);
         parser.require(XmlPullParser.END_TAG, ns, "description");
         return summary;
     }
 
-    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException
+    {
         String result = "";
         if (parser.next() == XmlPullParser.TEXT) {
             result = parser.getText();
@@ -108,7 +114,8 @@ public final class XmlParser
         return result;
     }
 
-    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException
+    {
         if (parser.getEventType() != XmlPullParser.START_TAG) {
             throw new IllegalStateException();
         }
